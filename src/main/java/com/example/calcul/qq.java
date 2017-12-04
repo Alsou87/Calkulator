786ec82da91bde7872c302e98f9ace57303ef75f
Alsou87 <34108875+Alsou87@users.noreply.github.com>
2017-12-05 02:44:25 +0300

    Add files via upload

---
+++ src/main/java/com/example/calcul/qq.java
@@ -0,0 +1,21 @@
+package com.example.calcul;
+import java.util.Scanner; //импортируем класс
+//@author ZiganshinaAlsu
+public class qq {
+    private static Scanner scanner;
+    public static void main (String[] args) {
+        scanner=new Scanner(System.in);
+        double a;
+        double b;
+        String operator;
+        System.out.println("Введите 1-ое слагаемое:");
+        a=scanner.nextDouble();
+        System.out.println("Введите 2-ое слагаемое:");
+        b=scanner.nextDouble();
+            System.out.printf("Cумма чисел равна  %.4f",+(a+b)); //округление
+        }
+
+    }
+
+
+
